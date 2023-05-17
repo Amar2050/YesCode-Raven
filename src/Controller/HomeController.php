b92ab69fd209a1e1752dc275628ea13854ae8b68
Amar2050 <amarboutayeb@gmail.com>
2023-05-17 11:43:37 +0200

    Entiity - Clean controller avant migration

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Fruit;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -11,9 +10,6 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home_page')]
     public function index(){
 
-        $banane = new Fruit();
-        $banane->setName("Bananeuh 2.2");
-        dump($banane);
         return $this->render('home/index.html.twig', [
         
         ]);
