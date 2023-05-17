8aa5d755c5c92462bd88eb23333e487d6b0543a9
Amar2050 <amarboutayeb@gmail.com>
2023-05-17 14:50:11 +0200

    Cleaning avant TP

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,19 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Fruit;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
-    {
-        // $product = new Product();
-        // $manager->persist($product);
+    public function load(ObjectManager $manager){
+
+
+        for ($i=0; $i < 10 ; $i++) { 
+    
+            // $manager->persist();
+        }
 
         $manager->flush();
     }
