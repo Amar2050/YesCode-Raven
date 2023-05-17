bf846e17622c0d2ae6cd198c786c82e62a0059f7
Amar2050 <amarboutayeb@gmail.com>
2023-05-17 13:51:27 +0200

    Entity - EntityManager - découverte

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,13 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Fruit;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home_page')]
-    public function index(){
+    public function index(EntityManagerInterface $manager){
+
+        $banana = new Fruit();
+        $banana->setName("Kiwi");
+
+        $something = new Fruit();
+        $something->setName("ArnOLJAyniREGINOother");
+
+        $manager->persist($banana);
+        $manager->persist($something);
+        $manager->flush();
 
         return $this->render('home/index.html.twig', [
         
