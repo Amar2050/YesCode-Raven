2e11b53142112fd1404e13e09e6e84764f1d2314
Amar2050 <amarboutayeb@gmail.com>
2023-05-17 14:04:18 +0200

    Clean de HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,23 +2,15 @@
 
 namespace App\Controller;
 
-use App\Entity\Fruit;
-use App\Repository\FruitRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home_page')]
-    public function index(FruitRepository $repo){
+    public function index(){
 
-        $fruits = $repo->findAll();
-
-        dump($fruits);
-        
-        return $this->render('home/index.html.twig', [
-        
-        ]);
+        return $this->render('home/index.html.twig', []);
     }
 
 }
