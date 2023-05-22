5f12ceaa55c930d531e645ee98ea553d64ceb260
Amar2050 <amarboutayeb@gmail.com>
2023-05-22 10:23:18 +0200

    Correction du TP-Back-End

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    #[Route('/articles', name: 'articles_index')]
+    public function index(ArticleRepository $repo): Response{
+
+        return $this->render('article/index.html.twig', [
+            "articles" => $repo->findAll()
+        ]);
+    }
+}
